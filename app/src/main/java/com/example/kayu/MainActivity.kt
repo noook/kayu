a295901d2a9e67615d1a34d9141ac91591172475
noook <me@neilrichter.com>
2019-06-24 10:28:58 +0200

    ✨ Added first view + Toast

--- app/src/main/java/com/example/kayu/MainActivity.kt
+++ app/src/main/java/com/example/kayu/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.kayu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextWatcher
+import android.support.v4.content.ContextCompat
 import android.util.Log
-import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fiche)
+        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.title_gradiant))
+        Toast.makeText(applicationContext,"Hello world :)",Toast.LENGTH_SHORT).show()
+        Log.v("MainActivity", "Hello world")
 
         /*findViewById<TextView>(R.id.my_text).setOnClickListener {
             Log.d("VIEW", "Clicked on app title !")
